125d16db5b984ce0798d1187f846a9ae28e13681
Apollon <apollllon721@gmail.com>
2019-10-23 09:20:50 +0000

    add error-messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    
+    def user_params
+      params.require(:user).permit(:f_name, :l_name, :email, :password,
+                                     :password_confirmation)
+    end
+  
 end

5168e9ee3a8016d18913d432810d3784c9370a86
Apollon <apollllon721@gmail.com>
2019-10-26 08:45:16 +0000

    Add romm show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,8 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  get 'room/index', to: 'room#index'
-  get 'room/new', to: 'room#new'
-  get 'room/edit', to: 'room#edit'
-  get 'room/show', to: 'room#show'
-
+  
   resources :users
-
+  resources :rooms
+  
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
-    assert_select "a[href=?]", room_index_path
+    # assert_select "a[href=?]", room_index_path
     # assert_select "a[href=?]", users_calender_path
     # assert_select "a[href=?]", users_edit_path
   end

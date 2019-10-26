7359506ad6da5ae76336988f7fad22ef450b0d5f
Apollon <apollllon721@gmail.com>
2019-10-26 11:29:41 +0000

    Add integration test named rooms-create

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,4 +6,17 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+  
+  def create
+    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+  
+  def room_params
+    params.require(:room).permit(:name, :capacity, :price, :open_at, :close_at)
+  end
 end
---
+++ test/integration/rooms_create_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RoomsCreateTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

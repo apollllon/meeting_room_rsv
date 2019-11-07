29dd084025bf2b582020fb1ec8546d04f4c2da1e
Apollon <apollllon721@gmail.com>
2019-11-07 23:05:53 +0000

    chenge migrate from schedules

--- db/migrate/20191101045941_change_data_stat_at_and_end_at_to_schedule.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataStatAtAndEndAtToSchedule < ActiveRecord::Migration[5.1]
-  def change
-    change_column :schedules, :start_at, :datetime
-    change_column :schedules, :end_at, :datetime
-  end
-end
--- db/migrate/20191107223320_remove_time_from_schedules.rb
+++ db/migrate/20191107223320_remove_time_from_schedules.rb
@@ -1,6 +1,6 @@
 class RemoveTimeFromSchedules < ActiveRecord::Migration[5.1]
   def change
-    remove_column :schedules, :start_at, :datetime
-    remove_column :schedules, :end_at, :datetime
+    remove_column :schedules, :start_at, :time
+    remove_column :schedules, :end_at, :time
   end
 end
